1c23bf6851f1c8c040a1ce927acf739dad0104eb
Quan <quan.liu@mpimet.mpg.de>
2022-07-29 11:07:31 +0200

    updated lon_height and lat_height func

--- src/pattern_statistic.py
+++ src/pattern_statistic.py
@@ -7,6 +7,23 @@ from eofs.standard import Eof
 from tqdm.notebook import trange, tqdm
 import seaborn as sn
 
+def select_co2(xarr):
+    """
+    select 3 CO_2 stages: 
+        1856: $1 \times CO_2$
+        1921: $2 \times CO_2$
+        1991: $4 \times CO_2$
+    **Arguments**
+        *xarr* the DataArray to be selected.
+    **Return**
+        *xarr* the DataArray only with three years.
+    """
+    co2time = ['1856-03-03','1921-03-16','1991-03-16']
+    co2time = pd.DatetimeIndex(co2time)
+    xarr_CO2 = xarr.sel(time = xarr.time.dt.year.isin(co2time.year))
+    return xarr_CO2
+
+
 def lon_height(eof,mode='EA'):
     """
     calculate zonally mean of eof
@@ -15,14 +32,9 @@ def lon_height(eof,mode='EA'):
     **Return**
         eof as a function of longitude and height
     """
-    # time
-    co2time = ['1856-03-03','1921-03-16','1991-03-16']
-    co2time = pd.DatetimeIndex(co2time)
-    eof_CO2 = eof.sel(time = eof.time.dt.year.isin(co2time.year))
-    
     # height
-    eof_height = eof_CO2.sel(hlayers = slice(20000,100000))
-    eof_height['hlayers'] = eof_height['hlayers']/100
+    eof_height = eof.sel(hlayers = slice(20000,100000))
+    eof_height['hlayers'] = eof_height['hlayers']/100  # from pa to hpa
 
     # lats
     eof_lat = eof_height.sel(lat = slice(70,40))
@@ -41,16 +53,11 @@ def lat_height(eof,mode = 'NAO'):
     **Return**
         eof as function of lattitude and height.
     """
-    # time
-    co2time = ['1856-03-03','1921-03-16','1991-03-16']
-    co2time = pd.DatetimeIndex(co2time)
-    eof_CO2 = eof.sel(time = eof.time.dt.year.isin(co2time.year))
     
     # height
-    eof_height = eof_CO2.sel(hlayers = slice(20000,100000))
+    eof_height = eof.sel(hlayers = slice(20000,100000))
     eof_height['hlayers'] = eof_height['hlayers']/100
 
-    
     # groupby
     lat_bins = np.arange(20,81,4)
     lat_labels = np.arange(22,81,4)
@@ -79,11 +86,14 @@ def pc_todf(xarr,name):
     df = df.rename(columns = {'__xarray_dataarray_variable__':name})
     return df
 
-def merge_df(dfs):
+def merge_df(dfs,on=['hlayers','mode','ens','time']):
+    """
+    merge dataframe along the selected columns. 
+    """
 
-    dfm = pd.merge(dfs[0],dfs[1],on =['hlayers','mode','ens','time'])
+    dfm = pd.merge(dfs[0],dfs[1],on = on)
 
-    dfm = pd.merge(dfm,dfs[2],on = ['hlayers','mode','ens','time'])
+    dfm = pd.merge(dfm,dfs[2],on = on)
 
     old_columns = dfs[0].columns[:4]
     new_columns = [dfs[0].columns[-1],dfs[1].columns[-1],dfs[2].columns[-1]]
