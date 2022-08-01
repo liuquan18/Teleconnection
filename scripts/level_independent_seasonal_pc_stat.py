b037a206d18026e8741cde8b2b6302f1626d5696
Quan <quan.liu@mpimet.mpg.de>
2022-08-01 09:45:15 +0200

    start the level_ind_seasonal-pc stat

---
+++ scripts/level_independent_seasonal_pc_stat.py
@@ -0,0 +1,26 @@
+"""
+This file generate the seasonal pc, from different fixed patterns,
+which is also different in all levels. 
+The scripts should generate 9 index.
+    spatial pattern: all    first10    last10
+    temporal pattern:
+    all               X       X           X
+    first10           X       X           X
+    last10            X       X           X
+"""
+# imports
+import numpy as np
+import pandas as pd
+import xarray as xr
+import matplotlib.pyplot as pld
+
+
+# Data load and pre-process
+#%%
+allens = xr.open_dataset("/work/mh0033/m300883/transition/gr19/gphSeason/allens_season_time.nc")
+# split ens
+splitens = sp.split_ens(allens)
+# demean ens-mean
+demean = splitens-splitens.mean(dim = 'ens')
+#select traposphere
+trop = demean.sel(hlayers = slice(20000,100000))
