7a8e90bcd91dcbf84b39cff4ad140685a4713729
Quan <quan.liu@mpimet.mpg.de>
2022-08-01 11:51:22 +0200

    change sp to ssp

--- scripts/level_independent_seas_pc.py
+++ scripts/level_independent_seas_pc.py
@@ -14,23 +14,26 @@ import numpy as np
 import pandas as pd
 import xarray as xr
 import matplotlib.pyplot as pld
+import seaborn as sns
 
 #%%
 import sys
 sys.path.append("..")
-import src.spatial_pattern as sp
-import src.pattern_statistic as ps
+import src.spatial_pattern as ssp
+import src.pattern_statistic as sps
+import src.index_statistic as sis
 
 import importlib
-importlib.reload(sp) # after changed the source code
-importlib.reload(ps)
+importlib.reload(ssp) # after changed the source code
+importlib.reload(sps)
+importlib.reload(sis)
 
 
 # Data load and pre-process
 #%%
 allens = xr.open_dataset("/work/mh0033/m300883/transition/gr19/gphSeason/allens_season_time.nc")
 # split ens
-splitens = sp.split_ens(allens)
+splitens = ssp.split_ens(allens)
 # demean ens-mean
 demean = splitens-splitens.mean(dim = 'ens')
 #select traposphere
@@ -39,14 +42,18 @@ trop = demean.sel(hlayers = slice(20000,100000))
 
 # first row: project all onto the three spatial patterns.
 # %%
-_,all_all,_ = sp.season_eof(trop.var156, nmode=2,window=10,
+_,all_all,_ = ssp.season_eof(trop.var156, nmode=2,window=10,
 fixed_pattern="all")  # "method" doesn't matter since the pc is 
                       # calculated independently.
-_, all_first,_ = sp.season_eof(trop.var156,nmode=2,window=10,
+_, all_first,_ = ssp.season_eof(trop.var156,nmode=2,window=10,
 fixed_pattern="first")
-_, all_last,_ = sp.season_eof(trop.var156,nmode=2,window=10,
+_, all_last,_ = ssp.season_eof(trop.var156,nmode=2,window=10,
 fixed_pattern="last")
 
+#%%
+all_all.name = 'pc'
+all_first.name = 'pc'
+all_last.name = 'pc'
 
 # second row: first 10 years of data projected on all, first10,and last10 eofs.
 # %%
@@ -63,4 +70,6 @@ last_last = all_last.isel(time = slice(-10,None))
 
 
 
+# %%
+all_dfs = sis.pc_column([all_all, all_first,all_last])
 # %%
