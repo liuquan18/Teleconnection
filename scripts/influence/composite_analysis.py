1a9542dcb2111dc6fdcb423f50d2e9b3a6fd9206
Quan <quan.liu@mpimet.mpg.de>
2022-08-31 09:27:35 +0200

    plot compositei field

--- scripts/influence/composite_analysis.py
+++ scripts/influence/composite_analysis.py
@@ -12,59 +12,117 @@ import src.plots.composite_plots as spcp
 
 #%%
 import importlib
+
 importlib.reload(spcp)
 importlib.reload(scp)
+
 # %%
-var = "precip"
-independent = "dep"
 
-dataname = (
-    "/work/mh0033/m300883/3rdPanel/data/influence/"
-    + var
-    + "/"
-    + "onepct_1850-1999_ens_1-100."
-    + var
-    + ".nc"
+
+def field_composite(var, independent="dep", hlayer=100000):
+    """
+    function to get the first and last composite mean field
+    """
+    dataname = (
+        "/work/mh0033/m300883/3rdPanel/data/influence/"
+        + var
+        + "/"
+        + "onepct_1850-1999_ens_1-100."
+        + var
+        + ".nc"
+    )
+
+    indexname = (
+        "/work/mh0033/m300883/3rdPanel/data/allPattern/"
+        + independent
+        + "_index_nonstd.nc"
+    )
+
+    changing_name = (
+        "/work/mh0033/m300883/3rdPanel/data/changingPattern/"
+        + independent
+        + "_index_nonstd.nc"
+    )
+
+    # Data
+    file = xr.open_dataset(dataname)
+    fdata = file[var]
+    # demean (ens-mean)
+    demean = fdata - fdata.mean(dim="ens")
+
+    # index
+    all_all_dep = xr.open_dataset(indexname).pc
+    changing_dep = xr.open_dataset(changing_name).pc
+    all_all_dep = all_all_dep.transpose("time", "ens", "mode", "hlayers")
+
+    mean_dep = all_all_dep.mean(dim="time")
+    std_dep = all_all_dep.std(dim="time")
+    dep_std = (changing_dep - mean_dep) / std_dep
+    index = dep_std.sel(hlayers=hlayer)
+
+    # change time
+    index["time"] = index.time.dt.year
+    demean["time"] = demean.time.dt.year
+
+    ComFirst = scp.composite(
+        index=index, data=demean, dim="mode", reduction="mean", period="first10"
+    )
+    ComLast = scp.composite(
+        index=index, data=demean, dim="mode", reduction="mean", period="last10"
+    )
+    return ComFirst, ComLast, ComLast - ComFirst
+
+
+#%%
+# Precip
+PrecipFirst, PrecipLast, PrecipDiff = field_composite("precip", "dep", hlayer=50000)
+
+print("precip")
+spcp.lastfirst_comp_map(PrecipFirst, PrecipLast, mode="NAO")
+
+spcp.lastfirst_comp_map(PrecipFirst, PrecipLast, mode="EA")
+
+#%%
+# t2max
+t2maxFirst, t2maxLast, t2maxDiff = field_composite("t2max", "dep", hlayer=100000)
+print("t2max")
+spcp.lastfirst_comp_map(
+    t2maxFirst,
+    t2maxLast,
+    mode="NAO",
+    levels=np.arange(-3, 3.1, 1),
+    unit="max 2m-temperature / K",
+)
+spcp.lastfirst_comp_map(
+    t2maxFirst,
+    t2maxLast,
+    mode="EA",
+    levels=np.arange(-3, 3.1, 1),
+    unit="max 2m-temperature / K",
 )
 
-indexname = (
-    "/work/mh0033/m300883/3rdPanel/data/allPattern/" + independent + "_index_nonstd.nc"
+#%%
+
+# tsurf
+tsurfFirst, tsurfLast, tsurfDiff = field_composite("tsurf", "dep", hlayer=100000)
+print("tsurf")
+spcp.lastfirst_comp_map(
+    tsurfFirst,
+    tsurfLast,
+    mode="NAO",
+    levels=np.arange(-3, 3.1, 0.5),
+    unit="surface temperature / K",
 )
 
-changing_name = (
-    "/work/mh0033/m300883/3rdPanel/data/changingPattern/"
-    + independent
-    + "_index_nonstd.nc"
+spcp.lastfirst_comp_map(
+    tsurfFirst,
+    tsurfLast,
+    mode="EA",
+    levels=np.arange(-3, 3.1, 0.5),
+    unit="surface temperature / K",
 )
 
-# Data
-file = xr.open_dataset(dataname)
-fdata = file[var]
-# demean (ens-mean)
-demean = fdata - fdata.mean(dim="ens")
-
-# index
-all_all_dep = xr.open_dataset(indexname).pc
-changing_dep = xr.open_dataset(changing_name).pc
-all_all_dep = all_all_dep.transpose("time", "ens", "mode", "hlayers")
-
-mean_dep = all_all_dep.mean(dim="time")
-std_dep = all_all_dep.std(dim="time")
-dep_std = (changing_dep - mean_dep) / std_dep
-index = dep_std.sel(hlayers=100000)
-
-# change time
-index["time"] = index.time.dt.year
-demean["time"] = demean.time.dt.year
 # %%
-ComAll = scp.composite(
-    index=index, data=demean, dim="mode", reduction="mean", period="all"
-)
-ComFirst = scp.composite(
-    index=index, data=demean, dim="mode", reduction="mean", period="first10"
-)
-ComLast = scp.composite(
-    index=index, data=demean, dim="mode", reduction="mean", period="last10"
-)
 
+spcp.lastfirst_comp_var(PrecipDiff,t2maxDiff,tsurfDiff,mode = 'NAO')
 # %%
--- src/plots/composite_plots.py
+++ src/plots/composite_plots.py
@@ -5,7 +5,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 from matplotlib.lines import Line2D
+
 import cartopy.crs as ccrs
+from cartopy.mpl.geoaxes import GeoAxes
+from cartopy.mpl.ticker import LongitudeFormatter, LatitudeFormatter
+from mpl_toolkits.axes_grid1 import AxesGrid
+import matplotlib.path as mpath
+from matplotlib.colorbar import Colorbar
+
+import matplotlib.ticker as mticker
+from cartopy.mpl.ticker import LongitudeFormatter, LatitudeFormatter, LatitudeLocator
+from cartopy.util import add_cyclic_point
+from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 
 def comp_count_plot(count: xr.DataArray, mode: str):
@@ -62,7 +73,27 @@ def vertical_profile(counts, mode="NAO"):
     ax.spines["left"].set_visible(True)
 
 
-def lastfirst_comp_map(first, last, mode, levels=np.arange(-2e-05, 2e-05, 0.2e-05)):
+def buildax(ax):
+    """
+    add grid coastline and gridlines
+    """
+    ax.set_global()
+    ax.coastlines(linewidth=0.5)
+    gl = ax.gridlines(crs=ccrs.PlateCarree(), draw_labels=False, linewidth=0.5)
+    gl.xformatter = LongitudeFormatter(zero_direction_label=False)
+    gl.xlocator = mticker.FixedLocator(np.arange(-180, 180, 45))
+
+    gl.ylocator = mticker.FixedLocator([20, 40, 60])
+    gl.yformatter = LatitudeFormatter()
+
+
+def lastfirst_comp_map(
+    first,
+    last,
+    mode,
+    levels=np.arange(-1.5e-05, 1.6e-05, 0.5e-05),
+    unit=r"precip / $m^{-2} s^{-2}$",
+):
     """
     rows for time (first10, last10)
     cols for extr_type (pos, neg)
@@ -71,29 +102,110 @@ def lastfirst_comp_map(first, last, mode, levels=np.arange(-2e-05, 2e-05, 0.2e-0
     proj = ccrs.Orthographic(central_longitude=-20, central_latitude=60)
 
     fig, axes = plt.subplots(
-        2, 2, figsize=(8, 8), dpi=500, subplot_kw={"projection": proj}
+        2, 3, figsize=(10, 8), dpi=500, subplot_kw={"projection": proj}
     )
 
-    data = [first.sel(mode=mode), last.sel(mode=mode)]
+    data = [
+        first.sel(mode=mode),
+        last.sel(mode=mode),
+        last.sel(mode=mode) - first.sel(mode=mode),
+    ]
     extr_type = ["pos", "neg"]
-    periods = ["first10", "last10"]
+    periods = ["first10", "last10", "last10 - first10"]
 
     for i, row in enumerate(axes):  # for extr_type
         for j, col in enumerate(row):  # for first10, last10.
             data_p = data[j].sel(extr_type=extr_type[i])
+            lats = data_p.lat
+            data_c, lons = add_cyclic_point(data_p, coord=data_p.lon, axis=-1)
+
             im = col.contourf(
-                data_p.lon,
-                data_p.lat,
-                data_p,
+                lons,
+                lats,
+                data_c,
                 levels=levels,
                 extend="both",
                 transform=ccrs.PlateCarree(),
                 cmap="RdBu_r",
             )
-            col.set_title(f"extreme:{extr_type[i]} period: {periods[j]}")
+            col.set_title(f"{extr_type[i]}  {periods[j]}")
+            buildax(col)
 
-    fig.subplots_adjust(hspace=0.05, wspace=0.05, right=0.8)
-    cbar_ax = fig.add_axes([0.85, 0.2, 0.03, 0.6])
-    fig.colorbar(im, cax=cbar_ax, label="precip-anomaly")
+    fig.subplots_adjust(hspace=-0.2, wspace=0.2, right=0.8)
+    cbar_ax = fig.add_axes([0.85, 0.25, 0.03, 0.5])
+    cbar = fig.colorbar(
+        im,
+        cax=cbar_ax,
+        label=unit,
+        ticks=levels,
+    )
+    if "precip" in unit:
+        cbar_ax.set_yticklabels(np.arange(-1.5, 1.6, 0.5).astype(str))
+        cbar_ax.set_title("1e-5", pad=20)
+
+    plt.show()
+
+
+def lastfirst_comp_var(
+    precip,
+    t2max,
+    tsurf,
+    mode,
+):
+    """
+    rows for time (first10, last10)
+    cols for extr_type (pos, neg)
+    """
+
+    proj = ccrs.Orthographic(central_longitude=-20, central_latitude=60)
+
+    fig, axes = plt.subplots(
+        2, 3, figsize=(10, 8), dpi=500, subplot_kw={"projection": proj}
+    )
+
+    data = [
+        precip.sel(mode=mode),
+        t2max.sel(mode=mode),
+        tsurf.sel(mode=mode),
+    ]
+
+    # some index
+    levels = [
+        np.arange(-1.5e-05, 1.6e-05, 0.5e-05),
+        np.arange(-3, 3.1, 1),
+        np.arange(-3, 3.1, 0.5),
+    ]
+    units = [r"precip / $m^{-2} s^{-2}$", "K", "K"]
+
+    extr_type = ["pos", "neg"]
+    vars = ["precip", "t2max", "tsurf"]
+    IM = []
+
+    for i, row in enumerate(axes):  # for extr_type
+        for j, col in enumerate(row):  # for vars.
+            data_p = data[j].sel(extr_type=extr_type[i])
+            lats = data_p.lat
+            data_c, lons = add_cyclic_point(data_p, coord=data_p.lon, axis=-1)
+
+            im = col.contourf(
+                lons,
+                lats,
+                data_c,
+                levels=levels[j],
+                extend="both",
+                transform=ccrs.PlateCarree(),
+                cmap="RdBu_r",
+            )
+            col.set_title(f"{extr_type[i]}  {vars[j]}")
+            buildax(col)
+
+            if i == 1:
+                divider = make_axes_locatable(col)
+                cax = divider.append_axes(
+                    "bottom", size="5%", pad="4%", map_projection=None
+                )
+                cbar = fig.colorbar(
+                    im, cax=cax, label=units[j], orientation="horizontal"
+                )
 
     plt.show()
