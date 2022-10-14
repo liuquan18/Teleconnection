9022f2383ebb62151be6e6339e08abc5beaad76d
Quan <quan.liu@mpimet.mpg.de>
2022-10-14 10:49:20 +0200

    chagne into small codes part

---
+++ src/Teleconnection/vertical_eof.py
@@ -0,0 +1,68 @@
+import numpy as np
+import pandas as pd
+import xarray as xr
+
+import src.Teleconnection.tools as tools
+import src.Teleconnection.rolling_eof as rolling_eof
+
+
+
+def vertical_eof(xarr, nmode, window=10, fixed_pattern="all", independent=True):
+    """
+    different way to do the eof vertically,
+    **Arguments**:
+        *xarr*: DataArry to decompose
+        *independent*: all layers decompose independently or not.
+                       if independent = True, the eof is applied independently over all levels.
+                       if independent = False, the vertical dimension is seen as one of the
+                       spatial dimension. so the eof stands for the common pattern of all layers.
+    """
+    if independent == True:
+        eof, pc, fra = independent_eof(xarr, nmode, window, fixed_pattern)
+    else:
+        eof, pc, fra = dependent_eof(xarr, nmode, window, fixed_pattern)
+
+    return eof, pc, fra
+
+def independent_eof(xarr, nmode, window, fixed_pattern):
+    """
+    do eof independently over all layers.
+    **Arguments**
+        *xarr* : the xarr to be composed.
+        *fixed_pattern* : the method to generate pc.
+        *method*: "rolling_eof" or "eof"
+    **Return**
+        EOF, PC and FRA.
+    """
+    eofs = []
+    pcs = []
+    fras = []
+
+    hlayers = xarr.hlayers
+    for h in tqdm(hlayers):
+        field = xarr.sel(hlayers=h)
+        eof, pc, fra = rolling_eof(field, nmode, window, fixed_pattern)
+
+        eofs.append(eof)
+        pcs.append(pc)
+        fras.append(fra)
+    eofs = xr.concat(eofs, dim=xarr.hlayers)
+    pcs = xr.concat(pcs, dim=xarr.hlayers)
+    fras = xr.concat(fras, dim=xarr.hlayers)
+    return eofs, pcs, fras
+
+
+def dependent_eof(xarr, nmode, window, fixed_pattern):
+    """
+    do eof independently over all layers.
+    **Arguments**
+        *xarr* : the xarr to be composed.
+        *fixed_pattern* : the method to generate pc.
+        *method*: "rolling_eof" or "eof"
+    **Return**
+        EOF, PC and FRA.
+    """
+    eofs, pcs, fras = rolling_eof(xarr, nmode, window, fixed_pattern)
+
+    return eofs, pcs, fras
+
