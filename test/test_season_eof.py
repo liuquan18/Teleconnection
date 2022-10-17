93a8dfe8eda35d935577e36ee947e90ca8d74785
Quan <quan.liu@mpimet.mpg.de>
2022-10-17 15:45:49 +0200

    new import for temporal index

--- test/test_season_eof.py
+++ test/test_season_eof.py
@@ -1,4 +1,4 @@
-#%%
+
 import importlib
 
 import numpy as np
@@ -11,16 +11,13 @@ import src.Teleconnection.tools as tools
 
 importlib.reload(season_eof)
 
-#%%
+
 ex = xr.open_dataset("/work/mh0033/m300883/3rdPanel/data/sample.nc")
 ex = ex.var156
 
-
-#%%
 eof,pc,fra = season_eof.season_eof(ex,nmode=2,
 window=10,fixed_pattern='all',independent = True,standard=True)
 
-# %%
 # test
 def test_season_eof():
     assert eof.hlayers == ex.hlayers
