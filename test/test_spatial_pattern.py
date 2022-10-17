9793c864789cc1235de8590a2cd71cfc385cf0fe
Quan <quan.liu@mpimet.mpg.de>
2022-10-17 14:56:32 +0200

    wired error

--- test/test_spatial_pattern.py
+++ test/test_spatial_pattern.py
@@ -1,27 +1,30 @@
+import importlib
+
+import numpy as np
+import pandas as pd
 import pytest
 import xarray as xr
-import pandas as pd
-import numpy as np
 
 import src.Teleconnection.spatial_pattern as ssp
 import src.Teleconnection.tools as tools
 
 # genrate random data
-time = pd.date_range('2010-10-01','2020-10-01',freq='Y')
-lon = np.linspace(-180,180,30)
-lat = np.linspace(-90,90,30)
+time = pd.date_range("2010-10-01", "2020-10-01", freq="Y")
+lon = np.linspace(-180, 180, 30)
+lat = np.linspace(-90, 90, 30)
 ens = np.arange(20)
-hlayers = np.linspace(1000,100,20)
-values = np.random.random((10,30,30,20,20)) # time, lon, lat, ens, hlayers
+hlayers = np.linspace(1000, 100, 20)
+values = np.random.random((10, 30, 30, 20, 20))  # time, lon, lat, ens, hlayers
 
-ex = xr.DataArray(values,dims = ['time','lon','lat','ens','hlayers'],
-coords = {'time':time,'lon':lon,'lat':lat,'ens':ens,'hlayers':hlayers}
+ex = xr.DataArray(
+    values,
+    dims=["time", "lon", "lat", "ens", "hlayers"],
+    coords={"time": time, "lon": lon, "lat": lat, "ens": ens, "hlayers": hlayers},
 )
-data =  ex.isel(hlayers = 0)
-data = tools.stack_ens(data, withdim='time')
-eof,pc,fra = ssp.doeof(data,nmode = 2)
+data = ex.isel(hlayers=0)
+data = tools.stack_ens(data, withdim="time")
+eof, pc, fra = ssp.doeof(data, nmode=2)
 
 # do eof
 def test_doeof():
-    assert fra >0
-
+    assert fra > 0
