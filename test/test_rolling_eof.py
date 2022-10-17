c7239976a3938103c2b281b93af7bba990490cf1
Quan <quan.liu@mpimet.mpg.de>
2022-10-17 14:56:50 +0200

    test for rolling_eof done

--- test/test_rolling_eof.py
+++ test/test_rolling_eof.py
@@ -1,30 +1,33 @@
-#%%
+import importlib
+
+import numpy as np
+import pandas as pd
 import pytest
 import xarray as xr
-import pandas as pd
-import numpy as np
 
 import src.Teleconnection.rolling_eof as rolling_eof
 import src.Teleconnection.tools as tools
 
-
+importlib.reload(rolling_eof)
 
 # genrate random data
-time = pd.date_range('2000-10-01','2020-10-01',freq='Y')
-lon = np.linspace(-180,180,30)
-lat = np.linspace(-90,90,30)
+time = pd.date_range("2000-10-01", "2020-10-01", freq="Y")
+lon = np.linspace(-180, 180, 30)
+lat = np.linspace(-90, 90, 30)
 ens = np.arange(20)
-hlayers = np.linspace(1000,100,20)
-values = np.random.random((20,30,30,20,20)) # time, lon, lat, ens, hlayers
+hlayers = np.linspace(1000, 100, 20)
+values = np.random.random((20, 30, 30, 20, 20))  # time, lon, lat, ens, hlayers
 
-ex = xr.DataArray(values,dims = ['time','lon','lat','ens','hlayers'],
-coords = {'time':time,'lon':lon,'lat':lat,'ens':ens,'hlayers':hlayers}
+ex = xr.DataArray(
+    values,
+    dims=["time", "lon", "lat", "ens", "hlayers"],
+    coords={"time": time, "lon": lon, "lat": lat, "ens": ens, "hlayers": hlayers},
 )
-# %%
 
-eof,pc,fra = rolling_eof.rolling_eof(ex,nmode = 2, window = 6, fixed_pattern = "False")
+# rolling eof on generated dataset.
+eof, pc, fra = rolling_eof.rolling_eof(ex, nmode=2, window=6, fixed_pattern="False")
 
-# %%
-eof,pc,fra = rolling_eof.rolling_eof(ex,nmode = 2, window = 6, fixed_pattern = 'all')
 
-# %%
+# test
+def test_rolling_eof():
+    assert len(ex) == 14
