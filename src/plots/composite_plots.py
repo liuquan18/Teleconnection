7d7314fe36f482e7002818a4f5830aec65990894
Quan <quan.liu@mpimet.mpg.de>
2022-10-24 15:21:18 +0200

    split to some small func

--- src/plots/composite_plots.py
+++ src/plots/composite_plots.py
@@ -8,8 +8,7 @@ import pandas as pd
 import seaborn as sns
 import xarray as xr
 from cartopy.mpl.geoaxes import GeoAxes
-from cartopy.mpl.ticker import (LatitudeFormatter, LatitudeLocator,
-                                LongitudeFormatter)
+from cartopy.mpl.ticker import LatitudeFormatter, LatitudeLocator, LongitudeFormatter
 from cartopy.util import add_cyclic_point
 from matplotlib.colorbar import Colorbar
 from matplotlib.lines import Line2D
@@ -96,12 +95,6 @@ def lastfirst_comp_map(
     cols for extr_type (pos, neg)
     """
 
-    proj = ccrs.Orthographic(central_longitude=-20, central_latitude=60)
-
-    fig, axes = plt.subplots(
-        2, 3, figsize=(10, 8), dpi=500, subplot_kw={"projection": proj}
-    )
-
     data = [
         first.sel(mode=mode),
         last.sel(mode=mode),
@@ -110,24 +103,24 @@ def lastfirst_comp_map(
     extr_type = ["pos", "neg"]
     periods = ["first10", "last10", "last10 - first10"]
 
+    fig, axes = axes_grid(2, 3)
+
     for i, row in enumerate(axes):  # for extr_type
         for j, col in enumerate(row):  # for first10, last10.
             data_p = data[j].sel(extr_type=extr_type[i])
-            lats = data_p.lat
-            data_c, lons = add_cyclic_point(data_p, coord=data_p.lon, axis=-1)
-
-            im = col.contourf(
-                lons,
-                lats,
-                data_c,
-                levels=levels,
-                extend="both",
-                transform=ccrs.PlateCarree(),
-                cmap="RdBu_r",
-            )
+            im = contourf(col, data_p, levels)
             col.set_title(f"{extr_type[i]}  {periods[j]}")
             buildax(col)
 
+    cbar_ax = cbar(levels, unit, fig, im)
+    if "precip" in unit:
+        cbar_ax.set_yticklabels(np.arange(-1.5, 1.6, 0.5).astype(str))
+        cbar_ax.set_title("1e-5", pad=20)
+
+    plt.show()
+
+
+def cbar(levels, unit, fig, im):
     fig.subplots_adjust(hspace=-0.2, wspace=0.2, right=0.8)
     cbar_ax = fig.add_axes([0.85, 0.25, 0.03, 0.5])
     cbar = fig.colorbar(
@@ -136,11 +129,35 @@ def lastfirst_comp_map(
         label=unit,
         ticks=levels,
     )
-    if "precip" in unit:
-        cbar_ax.set_yticklabels(np.arange(-1.5, 1.6, 0.5).astype(str))
-        cbar_ax.set_title("1e-5", pad=20)
 
-    plt.show()
+    return cbar_ax
+
+
+def contourf(ax, data, levels):
+    lats = data.lat
+    data_c, lons = add_cyclic_point(data, coord=data.lon, axis=-1)
+
+    im = ax.contourf(
+        lons,
+        lats,
+        data_c,
+        levels=levels,
+        extend="both",
+        transform=ccrs.PlateCarree(),
+        cmap="RdBu_r",
+    )
+
+    return im
+
+
+def axes_grid(
+    nrow, ncol, proj=ccrs.Orthographic(central_longitude=-20, central_latitude=60)
+):
+    fig, axes = plt.subplots(
+        nrow, ncol, figsize=(10, 8), dpi=500, subplot_kw={"projection": proj}
+    )
+
+    return fig, axes
 
 
 def lastfirst_comp_var(
