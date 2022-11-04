bfa0217d7b5af60cfc35fe84dbbfca1974f70b7f
Quan <quan.liu@mpimet.mpg.de>
2022-11-04 11:25:42 +0100

    add dim

--- src/extreme/period_pattern_extreme.py
+++ src/extreme/period_pattern_extreme.py
@@ -33,27 +33,31 @@ def period_extreme(peiod_index: xr.DataArray):
     return period_extreme
 
 
-def _period_extreme_count(period_extreme: xr.DataArray,dim:tuple):
+def _period_extreme_count(period_extreme: xr.DataArray, dim: tuple):
     """
     count the number of the extremes
     **Arguments**
         *period_extrme* the index with non-extreme masked as np.nan from period_extreme function
+        *dim* along which dim to count the extremes
     **Return**
         *extreme_count* the count of the extremes
     """
-    return ext.count_extreme(period_extreme,dim = dim)
+    return ext.count_extreme(period_extreme, dim=dim)
 
 
-def period_extreme_count(period_period: xr.DataArray, all_all: xr.DataArray):
+def period_extreme_count(
+    period_period: xr.DataArray, all_all: xr.DataArray, dim=("time", "ens")
+):
     """
     the extreme count of ten_year period
     **Arguments**
         *period_period* the index of the first10-first or last10-last
         *all_all* the index of all-all pattern
+        *dim* along which dim to count.
     **Return**
         *extreme_count* the count of the extremes in this ten years.
     """
     period_index = normalize(period_period, all_all)
     extreme = period_extreme(period_index)
-    count = _period_extreme_count(extreme,dim = ('time','ens'))
+    count = _period_extreme_count(extreme, dim=dim)
     return count
