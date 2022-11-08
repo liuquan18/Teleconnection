bedadde30057902cbafde5db57bef6b8df913ac3
Quan <quan.liu@mpimet.mpg.de>
2022-11-08 17:44:14 +0100

    composite pass

--- src/composite/composite.py
+++ src/composite/composite.py
@@ -26,10 +26,9 @@ def extreme(
     return extreme
 
 
-def _composite(index, data, reduction="mean"):
+def composite(index, data, reduction="mean",dim = 'com'):
     """
-    the composite mean or count of data, determined by the extreme state
-    of index.
+    the composite mean or count of data, in terms of different extreme type.
         - stack to one series.
         - for pos and neg:
             - get the extreme index
@@ -43,122 +42,17 @@ def _composite(index, data, reduction="mean"):
     **Return**
         *extreme_composite* the mean field or counts of extreme cases.
     """
-    try:
-        data = data.sel(hlayers=index.hlayers)
-    except KeyError:
-        data = data
-    data = data.stack(com=("time", "ens"))
-    index = index.stack(com=("time", "ens"))
-
     extreme_composite = []
     extreme_type = xr.DataArray(["pos", "neg"], dims=["extr_type"])
     for extr_type in extreme_type.values:
         extr_index = extreme(index, extreme_type=extr_type)
         extr_data = data.where(extr_index)
         if reduction == "mean":
-            composite = extr_data.mean(dim="com")
+            composite = extr_data.mean(dim=dim)
         elif reduction == "count":
-            composite = extr_index.count(dim="com")
+            composite = extr_index.count(dim=dim)
         extreme_composite.append(composite)
     extreme_composite = xr.concat(extreme_composite, dim=extreme_type)
 
     return extreme_composite
 
-
-def composite(
-    index: xr.DataArray,
-    data: xr.DataArray,
-    dim: str = "hlayers",
-    reduction: str = "mean",
-    period: str = "all",
-):
-    """
-    composite mean maps of extreme cases or counts of extreme cases.
-    given the index and the data, determine the time and ens coordinates
-    where extreme cases happen from the index, and select the data with
-    those indexes, average the selected fields.
-    **Arguments**
-        *index* the index of NAO and EA
-        *data* the original geopotential data.
-        *reduction* mean or count
-        *period* 'first10','last10','all'
-    **Return**
-        *compostie* the composite mean of the extreme cases.
-    """
-    if period == "all":
-        index = index
-    elif period == "first10":
-        index = index.isel(
-            time=slice(0, 10)
-        )  # the index actually started from 1856,so wrong here.
-    elif period == "last10":
-        index = index.isel(time=slice(-10, None))
-    data = data.sel(time=index.time)
-
-    Composite = []
-    for mode in index.mode:
-        _index = index.sel(mode=mode)
-        if dim == "hlayers":
-            composite = _index.groupby(dim).apply(
-                _composite, data=data, reduction=reduction
-            )
-        elif dim == "mode":
-            composite = _composite(_index, data, reduction)
-        Composite.append(composite)
-    Composite = xr.concat(Composite, dim=index.mode)
-
-    return Composite
-
-
-def field_composite(var, independent="dep", hlayer=100000):
-    """
-    function to get the first and last composite mean field
-    """
-    dataname = (
-        "/work/mh0033/m300883/3rdPanel/data/influence/"
-        + var
-        + "/"
-        + "onepct_1850-1999_ens_1-100."
-        + var
-        + ".nc"
-    )
-
-    indexname = (
-        "/work/mh0033/m300883/3rdPanel/data/allPattern/"
-        + independent
-        + "_index_nonstd.nc"
-    )
-
-    changing_name = (
-        "/work/mh0033/m300883/3rdPanel/data/changingPattern/"
-        + independent
-        + "_index_nonstd.nc"
-    )
-
-    # Data
-    file = xr.open_dataset(dataname)
-    fdata = file[var]
-    # demean (ens-mean)
-    demean = fdata - fdata.mean(dim="ens")
-
-    # index
-    all_all_dep = xr.open_dataset(indexname).pc
-    changing_dep = xr.open_dataset(changing_name).pc
-    all_all_dep = all_all_dep.transpose("time", "ens", "mode", "hlayers")
-
-    mean_dep = all_all_dep.mean(dim="time")
-    std_dep = all_all_dep.std(dim="time")
-    dep_std = (changing_dep - mean_dep) / std_dep
-    index = dep_std.sel(hlayers=hlayer)
-
-    # change time
-    index["time"] = index.time.dt.year
-    demean["time"] = demean.time.dt.year
-
-    ComFirst = composite(
-        index=index, data=demean, dim="mode", reduction="mean", period="first10"
-    )
-    ComLast = composite(
-        index=index, data=demean, dim="mode", reduction="mean", period="last10"
-    )
-    return ComFirst, ComLast, ComLast - ComFirst
--- test/test_composite.py
+++ test/test_composite.py
@@ -3,26 +3,32 @@ import xarray as xr
 import numpy as np
 import pandas as pd
 import src.composite.composite as comp
+import pytest
 
 # %%
-
+import importlib
+importlib.reload(comp)
 #%%
 # genrate random temporal index
-period_time = pd.date_range("2010-10-01", "2021-10-01", freq="Y")
+time = pd.date_range("2010-10-01", "2021-10-01", freq="Y")
 
-period_values = np.arange(-5, 6, 1)
+values_index = np.arange(-5, 6, 1)
 np.random.seed(0)
 
-index = xr.DataArray(period_values, dims=["time"], coords={"time": period_time})
+index = xr.DataArray(values_index, dims=["time"], coords={"time": time})
 
 # %%
 
-values_data = np.random.randn(11,10,10)
+values_data = np.ones((11,10,10))
 lon = np.arange(10)
 lat = np.arange(10)
-data = xr.DataArray(values_data,dims = ['time','lat','lon'],coords = {'time':period_time,'lat':lat,'lon':lon})
+data = xr.DataArray(values_data,dims = ['time','lat','lon'],coords = {'time':time,'lat':lat,'lon':lon})
 
 
 # %%
-comp._composite(index,data)
+composite = comp.composite(index,data,dim = 'time')
+# %%
+# test
+def test_composite():
+    assert composite.mean().values == 1
 # %%
