3ef9328ad5bda5dbd5d5d5aacb24952855125caa
Quan <quan.liu@mpimet.mpg.de>
2022-11-09 16:29:56 +0100

    go back to standard after unstack

--- src/Teleconnection/spatial_pattern.py
+++ src/Teleconnection/spatial_pattern.py
@@ -179,13 +179,12 @@ def project_field(fieldx, eofx, dim="com", standard=True):
             },
         )
     PPC.name = "pc"
-    # standardize pc
-    mean = PPC.mean(dim="com")
-    std = PPC.mean(dim="com")
-    PPC = (PPC - mean) / std
+
     # to unstack 'com' to 'time' and 'ens' if 'com' exists.
     PPC = PPC.unstack()
 
+    if standard:
+        PPC = tools.standardize(PPC)
     return PPC
 
 
