5f2bdf555f520cf76c6fc6a92f5a00a81f55de1f
Quan <quan.liu@mpimet.mpg.de>
2022-11-09 16:30:18 +0100

    test

--- test/test_spatial_pattern.py
+++ test/test_spatial_pattern.py
@@ -1,3 +1,4 @@
+#%%
 import importlib
 
 import numpy as np
@@ -8,23 +9,39 @@ import xarray as xr
 import src.Teleconnection.spatial_pattern as ssp
 import src.Teleconnection.tools as tools
 
+#%%
+import importlib
+importlib.reload(ssp)
+
+#%%
 # genrate random data
 time = pd.date_range("2010-10-01", "2020-10-01", freq="Y")
-lon = np.linspace(-180, 180, 30)
-lat = np.linspace(-90, 90, 30)
-ens = np.arange(20)
-hlayers = np.linspace(1000, 100, 20)
-values = np.random.random((10, 30, 30, 20, 20))  # time, lon, lat, ens, hlayers
+lon = np.linspace(-180, 180, 10)
+lat = np.linspace(-90, 90, 10)
+ens = [1,2,3]
 
-ex = xr.DataArray(
+np.random.seed(0)
+values = np.random.random((10, 10, 10, 3))  # time, lon, lat, ens
+
+data = xr.DataArray(
     values,
-    dims=["time", "lon", "lat", "ens", "hlayers"],
-    coords={"time": time, "lon": lon, "lat": lat, "ens": ens, "hlayers": hlayers},
+    dims=["time", "lon", "lat", "ens"],
+    coords={"time": time, "lon": lon, "lat": lat, "ens": ens},
 )
-data = ex.isel(hlayers=0)
-data = tools.stack_ens(data, withdim="time")
-eof, pc, fra = ssp.doeof(data, nmode=2)
 
+
+#%%
+s_data = tools.stack_ens(data, withdim="time")
+eof, pc, fra = ssp.doeof(s_data, nmode=2,dim = 'com',standard=True)
+
+#%%
+ppc = ssp.project_field(s_data,eof,dim = 'com',standard = True)
+
+#%%
 # do eof
 def test_doeof():
-    assert fra.sum().values > 0
+    assert pc.std().values == 1.
+    assert ppc.std().values == 1.
+
+    
+
