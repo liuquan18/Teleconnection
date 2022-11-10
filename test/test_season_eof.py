b6e08b848a4be0e2092b05807f6fd609b70ef53f
Quan <quan.liu@mpimet.mpg.de>
2022-11-10 11:28:10 +0100

    season_eof passed

--- test/test_season_eof.py
+++ test/test_season_eof.py
@@ -1,4 +1,4 @@
-
+#%%
 import importlib
 
 import numpy as np
@@ -11,18 +11,19 @@ import src.Teleconnection.tools as tools
 
 importlib.reload(season_eof)
 
+#%%
 # #%% real data
 # ex = xr.open_dataset("/work/mh0033/m300883/3rdPanel/data/sample.nc")
 # ex = ex.var156
 
 
 # genrate random data
-time = pd.date_range("2010-10-01", "2020-10-01", freq="Y")
-lon = np.linspace(-180, 180, 30)
-lat = np.linspace(-90, 90, 30)
-ens = np.arange(20)
-hlayers = np.linspace(1000, 100, 20)
-values = np.random.random((10, 30, 30, 20, 20))  # time, lon, lat, ens, hlayers
+time = pd.date_range("2000-10-01", "2020-10-01", freq="Y")
+lon = np.linspace(-180, 180, 10)
+lat = np.linspace(-90, 90, 10)
+ens = np.arange(3)
+hlayers = np.linspace(1000, 100, 2)
+values = np.random.random((20, 10, 10, 3, 2))  # time, lon, lat, ens, hlayers
 
 ex = xr.DataArray(
     values,
@@ -30,9 +31,19 @@ ex = xr.DataArray(
     coords={"time": time, "lon": lon, "lat": lat, "ens": ens, "hlayers": hlayers},
 )
 
+#%%
 eof,pc,fra = season_eof.season_eof(ex,nmode=2,
 window=6,fixed_pattern='all',independent = True,standard=True)
 
+#%%
+eof,pc,fra = season_eof.season_eof(ex,nmode=2,
+window=6,fixed_pattern='all',independent = True,standard=False)
+
+
+#%%
+eof,pc,fra = season_eof.season_eof(ex,nmode=2,
+window=6,fixed_pattern='first',independent = False,standard=True)
+#%%
 # test
 def test_season_eof():
-    assert fra.sum().values > 0
+    assert pc.std().values == 1.0
